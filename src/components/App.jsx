2b66e2e305e29d6af63d1f78b11337d55ef1a438
Hack Reactor Student <student@hackreactor.com>
2017-07-12 14:51:22 -0500

    dynamic video list and video list entry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
 var App = () => (
   <div>
+    <h1>Hello</h1>
     <Nav />
     <div className="col-md-7">
       <VideoPlayer/>
     </div>
     <div className="col-md-5">
-      <VideoList/>
+      <VideoList videos={window.exampleVideoData} />
     </div>
   </div>
+
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+ReactDOM.render(<App />, document.getElementById('app'));
+
